e9281222ae7649d85da5244fd07522eccfdd71e5
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-22 14:00:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
   const sorted = array.toSorted((a, b) => a - b);
-  const mid = Math.floor(sorted.length / 2);
-
-  if (sorted.length % 2 === 0) {
-    return getMean([sorted[mid - 1], sorted[mid]]);
-  } else {
-    return sorted[mid];
-  }
-}
-
-function getMean(arr) {
-  return arr.reduce((a, b) => a + b, 0) / arr.length;
+  const median =
+    sorted.length % 2 === 0
+      ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
+      : sorted[Math.floor(sorted.length / 2)];
+  return median;
 }
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-  
-  const mean = getMean(numbers);
 
-  document.querySelector("#mean").textContent = mean;
+const mean = getMean(numbers);
+document.querySelector("#mean").textContent = mean.toFixed(2);
+
+const median = getMedian(numbers);
+document.querySelector("#median").textContent = median;
 }
