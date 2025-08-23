4ae1b1bc49b0fd0ce3b8729e235f7bfd39eadfa4
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-23 12:19:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-body {
-  margin: 0;
-  background-color: rgb(27, 27, 50);
-  text-align: center;
-  color: #fff;
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const getMedian = (array) => {
+  const sorted = array.toSorted((a, b) => a - b);
+  const median =
+    sorted.length % 2 === 0
+      ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
+      : sorted[Math.floor(sorted.length / 2)];
+  return median;
 }
 
-button {
-  cursor: pointer;
-  background-color: rgb(59, 59, 79);
-  border: 3px solid white;
-  color: white;
+
+const getMode = (array) => {
+  const counts = {};
+  array.forEach(el => counts[el] = counts[el] ? counts[el] + 1 : 1);
+  console.log(counts)
+  return counts;
 }
 
-input {
-  background-color: rgb(10, 10, 35);
-  color: white;
-  border: 1px solid rgb(59, 59, 79);
-}
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+  
+  const mean = getMean(numbers);
+  const median = getMedian(numbers);
 
-.bold {
-  font-weight: bold;
+  document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
 }
