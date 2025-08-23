6aab7221963c71048787b63aa3993451056effa0
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-23 12:14:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+const getMean = array => array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
   const sorted = array.toSorted((a, b) => a - b);
@@ -8,19 +8,23 @@ const getMedian = (array) => {
       : sorted[Math.floor(sorted.length / 2)];
   return median;
 }
+
+const getMode = array => {
+  const counts = {};
 const getMode = (array) => {
   const counts = {}
+  return array
 }
-
+};
 
 const calculate = () => {
-  const value = document.querySelector("#numbers").value;
+  const value = document.querySelector('#numbers').value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-  
+
   const mean = getMean(numbers);
   const median = getMedian(numbers);
 
-  document.querySelector("#mean").textContent = mean;
-  document.querySelector("#median").textContent = median;
-}
+  document.querySelector('#mean').textContent = mean;
+  document.querySelector('#median').textContent = median;
+};
