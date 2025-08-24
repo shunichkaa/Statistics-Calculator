a69ba43a728d6b136f4234f3e068ab42f69c624f
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-24 11:30:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,8 @@ const getRange = (array) => {
 
 const getVariance = (array) => {
   const mean = getMean(array);
-  const differences = array.map((el) => el - mean);
+  const differences = array.map(el => el - mean);
+  const squaredDifferences = differences.map(el => el ** 2);
 };
 
 const calculate = () => {
