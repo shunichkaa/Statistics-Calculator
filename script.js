e7591d524bfa9ec98382b6a93609340850f176a3
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-26 12:13:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ const getVariance = (array) => {
 const getStandardDeviation = (array) => {
   const variance = getVariance(array);
   const standardDeviation = Math.sqrt(variance);
-return standardDeviation
+  return standardDeviation;
 }
 
 const calculate = () => {
@@ -51,15 +51,17 @@ const calculate = () => {
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
   
-  const mean = getMean(numbers);
-  const median = getMedian(numbers);
-  const mode = getMode(numbers);
-  const range = getRange(numbers);
-  const variance = getVariance(numbers);
+const mean = getMean(numbers);
+const median = getMedian(numbers);
+const mode = getMode(numbers);
+const range = getRange(numbers);
+const variance = getVariance(numbers);
+const standardDeviation = getStandardDeviation(numbers);
 
-  document.querySelector("#mean").textContent = mean;
-  document.querySelector("#median").textContent = median;
-  document.querySelector("#mode").textContent = mode;
-  document.querySelector("#range").textContent = range;
-  document.querySelector("#variance").textContent = variance;
+document.querySelector("#mean").textContent = mean;
+document.querySelector("#median").textContent = median;
+document.querySelector("#mode").textContent = mode;
+document.querySelector("#range").textContent = range;
+document.querySelector("#variance").textContent = variance;
+document.querySelector("#standardDeviation").textContent = standardDeviation;
 }
