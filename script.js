f054b250ec9a7731a0ec5777dc417d410c36bb77
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-26 12:08:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -40,20 +40,24 @@ const getVariance = (array) => {
   return variance;
 }
 
+const getStandardDeviation = (array) => {
+  const variance = getVariance(array);
+};
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
   
-const mean = getMean(numbers);
-const median = getMedian(numbers);
-const mode = getMode(numbers);
-const range = getRange(numbers);
-const variance = getVariance(numbers);
+  const mean = getMean(numbers);
+  const median = getMedian(numbers);
+  const mode = getMode(numbers);
+  const range = getRange(numbers);
+  const variance = getVariance(numbers);
 
-document.querySelector("#mean").textContent = mean;
-document.querySelector("#median").textContent = median;
-document.querySelector("#mode").textContent = mode;
-document.querySelector("#range").textContent = range;
-document.querySelector("#variance").textContent = variance;
+  document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
+  document.querySelector("#range").textContent = range;
+  document.querySelector("#variance").textContent = variance;
 }
