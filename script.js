b56dbe67a5610207d04f298799980abe13906a1c
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-26 12:07:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -38,20 +38,22 @@ const getVariance = (array) => {
     return acc + squared;
   }, 0) / array.length;
   return variance;
-};
+}
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+  
+const mean = getMean(numbers);
+const median = getMedian(numbers);
+const mode = getMode(numbers);
+const range = getRange(numbers);
+const variance = getVariance(numbers);
 
-  const mean = getMean(numbers);
-  const median = getMedian(numbers);
-  const mode = getMode(numbers);
-  const range = getRange(numbers);
-
-  document.querySelector("#mean").textContent = mean;
-  document.querySelector("#median").textContent = median;
-  document.querySelector("#mode").textContent = mode;
-  document.querySelector("#range").textContent = range;
+document.querySelector("#mean").textContent = mean;
+document.querySelector("#median").textContent = median;
+document.querySelector("#mode").textContent = mode;
+document.querySelector("#range").textContent = range;
+document.querySelector("#variance").textContent = variance;
 }
